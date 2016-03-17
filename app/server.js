9ecf7666c7100a7c4e8c75640263f6e3487d1947
Cameron Zaas <cameronzaas@gmail.com>
2016-03-16 20:12:32 -0700

    Basic universal react app

---
+++ app/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import http from 'http';
+
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+
+import { AppComponent } from './components/app-component.js';
+import { IndexComponent } from './components/pages/index-component.js';
+
+const routes = {
+	path: '',
+	component: AppComponent,
+	childRoutes: [
+		{
+			path: '/',
+			component: IndexComponent
+		}
+	]
+};
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+app.get('*', (req, res) => {
+	match({ routes, location: req.url }, (err, redirectLocation, props) => {
+		if (err) {
+			res.status(500).send(err.message);
+		} else if (redirectLocation) {
+			res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+		} else if (props) {
+			const markup = renderToString(<RouterContext {...props} />);
+
+			res.render('index', { markup });
+		} else {
+			res.sendStatus(404);
+		}
+	});
+});
+
+app.listen(3000, () => console.log('running on port 3000'));
